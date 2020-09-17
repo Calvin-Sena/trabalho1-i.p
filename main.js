708a71b5ae466613247567bc759edcefaabcb47e
Calvin Sena Silva Batista <70851971+Calvin-Sena@users.noreply.github.com>
2020-09-17 10:03:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function clicar() {
+
+    var n1 = document.querySelector(".n1").value;
+    var n2 = document.querySelector(".n2").value;
+
+    var resultado = n1 * n2;
+    document.querySelector(".resultado").innerHTML = resultado; 
+}
